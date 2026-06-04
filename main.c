fd606c72bab45bd9889ba586841ff2d9afb92efa
sugar <panhnasak20@gmail.com>
2026-06-04 15:33:40 +0700

    exe

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include "connector.h"
 
 int main(){
-    printf("Team: return 404");
-    printf("%i", add(1, 3));
-    printf("hi");
+    printf("Team: return 404\n");
+    printf("%i\n", add(2, 3));
+    printf("hi\n");
     return 0;
 }
